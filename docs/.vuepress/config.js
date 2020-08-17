3780111aa2b177c5dbe5444913abff4b0fe9ad40
microJ <clear2Jay@gmail.com>
2020-08-17 12:55:38 +0800

    feat: update favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,49 @@
-const themeConfig = require('./config/theme/')
+const themeConfig = require("./config/theme/");
 
 module.exports = {
   title: "叫我小J",
-  description: 'Record good times.',
-  dest: 'public',
+  description: "Record good times.",
+  dest: "public",
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
-  theme: 'reco',
+  theme: "reco",
   themeConfig,
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
-  plugins: ['@vuepress/medium-zoom', 'flowchart'] 
-}  
+  plugins: ["@vuepress/medium-zoom", "flowchart"],
+};

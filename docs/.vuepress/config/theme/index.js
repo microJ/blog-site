8228d47147752f6ab4ca58bb34752641fab868fa
microJ <clear2Jay@gmail.com>
2020-08-25 10:29:16 +0800

    feat: 使用 DesertsP/Valine-Admin 支持邮件提醒,垃圾评论过滤

--- docs/.vuepress/config/theme/index.js
+++ docs/.vuepress/config/theme/index.js
@@ -14,8 +14,8 @@ module.exports = Object.assign({}, themeReco, {
   subSidebar: "auto",
   authorAvatar: "/logo.png",
   valineConfig: {
-    appId: '7pWKyFjLwVXJAIcndpUY6ep3-gzGzoHsz',// your appId
-    appKey: 'hpI8Qqyp9AL0L6QKU7EYlHjm', // your appKey
+    appId: 'lvAGbPUQenlPVV4UxH4FALgD-MdYXbMMI',// your appId
+    appKey: 'y30ySubnlyt3h4BXfDexKese', // your appKey
     placeholder: '我那么多期盼，那么多遗憾，你知道吗~~',
     avatar: 'monsterid',
     meta: ['nick', 'mail'],

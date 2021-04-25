335538b5061fb4b37512522b2be0c810551f896c
microJ <clear2Jay@gmail.com>
2021-04-25 12:39:57 +0800

    chore: SEO optimization

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,65 +1,79 @@
-const themeConfig = require("./config/theme/");
-const path = require("path");
+const themeConfig = require('./config/theme/')
+const path = require('path')
+
+const autometa_options = {
+  site: {
+    name: '叫我小J',
+  },
+  canonical_base: 'https://blog.expect2.cyou',
+}
 
 module.exports = {
-  title: "叫我小J",
-  description: "Record good times.",
-  dest: "public",
+  title: '叫我小J',
+  description: 'Record good times.',
+  dest: 'public',
   head: [
     [
-      "link",
+      'link',
       {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/apple-touch-icon.png"
-      }
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
     ],
     [
-      "link",
+      'link',
       {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/favicon-32x32.png"
-      }
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
     ],
     [
-      "link",
+      'link',
       {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/favicon-16x16.png"
-      }
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
     ],
-    ["link", { rel: "manifest", href: "/site.webmanifest" }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
     [
-      "meta",
+      'meta',
       {
-        name: "viewport",
-        content: "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1,user-scalable=no',
+      },
+    ],
   ],
-  theme: "reco",
+  theme: 'reco',
   themeConfig,
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   plugins: [
-    "@vuepress/medium-zoom",
-    "flowchart",
     [
-      "@vuepress-reco/vuepress-plugin-rss",
+      'sitemap',
       {
-        site_url: "https://blog.expect2.cyou"
-      }
-    ]
+        hostname: 'https://blog.expect2.cyou',
+        // 排除无实际内容的页面
+        exclude: ['/404.html'],
+      },
+    ],
+    ['autometa', autometa_options],
+    '@vuepress/medium-zoom',
+    'flowchart',
+    [
+      '@vuepress-reco/vuepress-plugin-rss',
+      {
+        site_url: 'https://blog.expect2.cyou',
+      },
+    ],
+    'vuepress-plugin-baidu-autopush',
   ],
   chainWebpack: config => {
-    config.resolve.alias.set(
-      "@images",
-      path.resolve(__dirname, "../views/assets/images")
-    );
-  }
-};
+    config.resolve.alias.set('@images', path.resolve(__dirname, '../views/assets/images'))
+  },
+}
